2f6993d931e8c27a69ff4363e0b3405d69d2f444
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-05-14 23:56:52 +0000

    Fix reused variable name

--- JS/toolbar.js
+++ JS/toolbar.js
@@ -3,8 +3,8 @@ const toolbar = 'https://team2550.github.io/HTML/toolbar.html';
 async function displayToolbar() {
     try {
         const response = await fetch(toolbar);
-        const toolbar = await response.text();
-        document.body.insertAdjacentHTML('afterbegin', toolbar);
+        const toolbarData = await response.text();
+        document.body.insertAdjacentHTML('afterbegin', toolbarData);
     } catch (error) {
         console.error('Unable to display toolbar', error);
     }
