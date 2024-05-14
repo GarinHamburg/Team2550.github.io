00ed1630823fde7e80c016b7ef4f98bad7acc35a
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-05-14 23:40:49 +0000

    Begin site rewrite

---
+++ JS/toolbar.js
@@ -0,0 +1,14 @@
+const toolbar = 'https://team2550.github.io/HTML/toolbar.html';
+
+async function displayToolbar() {
+    try {
+        const response = await fetch(toolbar);
+        const toolbar = await response.text;
+        document.body.insertAdjacentHTML('afterbegin', toolbar);
+    } catch (error) {
+        console.error('Unable to display toolbar', error);
+    }
+}
+
+displayToolbar();
+
