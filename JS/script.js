8f7a277ea6bd293a7b5bd711e910ee326c46019c
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-05-30 23:41:09 +0000

    Add the second image

--- JS/script.js
+++ JS/script.js
@@ -18,13 +18,19 @@ async function sponsorbar() {
             var src = "https://garinhamburg.github.io/Team2550.github.io/Sponsors/SponsorLogos/" + parsedData[i].image;
             var url = parsedData[i].url;
             var sponsorImage = new Image;
+            var sponsorImage2 = new Image;
             sponsorImage.src = src;
             sponsorImage.alt = alt;
             sponsorImage.href = url;
+            sponsorImage.className = "item";
+            sponsorImage2.src = src;
+            sponsorImage2.alt = alt;
+            sponsorImage2.href = url;
+            sponsorImage2.className = "item";
             var firstLoop = document.getElementById("scroll-one");
             var secondLoop = document.getElementById("scroll-two");
             firstLoop.appendChild(sponsorImage);
-            secondLoop.appendChild(sponsorImage);
+            secondLoop.appendChild(sponsorImage2);
         } 
     } catch (error) {
         console.error('Unable to fetch sponsors', error);
