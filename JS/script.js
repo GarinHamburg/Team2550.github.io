f32aa204ab65ddd11da4465207c8ad59e6a36bd2
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-06-04 22:57:25 +0000

    Implement No-Logo Sponsors

--- JS/script.js
+++ JS/script.js
@@ -14,33 +14,66 @@ async function sponsorbar() {
         // For right now though, it works
         // -G
         for (var i = 0; i < parsedData.length; i++) {
-            var alt = parsedData[i].alt;
-            var src = "https://garinhamburg.github.io/Team2550.github.io/Sponsors/SponsorLogos/" + parsedData[i].image;
-            var url = parsedData[i].url;
-            var sponsorImage = new Image;
-            var sponsorImage2 = new Image;
-            sponsorImage.src = src;
-            sponsorImage.alt = alt;
-            sponsorImage2.src = src;
-            sponsorImage2.alt = alt;
-            var firstLoop = document.getElementById("scroll-one");
-            var secondLoop = document.getElementById("scroll-two");
-            if (url != null) {
-                var urlContainer = document.createElement("a");
-                urlContainer.href = url;
-                var urlContainer2 = document.createElement("a");
-                urlContainer2.href = url;
-                urlContainer.className = "item";
-                urlContainer2.className = "item";
-                urlContainer.appendChild(sponsorImage);
-                urlContainer2.appendChild(sponsorImage2);
-                firstLoop.appendChild(urlContainer);
-                secondLoop.appendChild(urlContainer2);
+            if (parsedData[i].type == "logo") {
+                var alt = parsedData[i].alt;
+                var src = "https://garinhamburg.github.io/Team2550.github.io/Sponsors/SponsorLogos/" + parsedData[i].image;
+                var url = parsedData[i].url;
+                var sponsorImage = new Image;
+                var sponsorImage2 = new Image;
+                sponsorImage.src = src;
+                sponsorImage.alt = alt;
+                sponsorImage2.src = src;
+                sponsorImage2.alt = alt;
+                var firstLoop = document.getElementById("scroll-one");
+                var secondLoop = document.getElementById("scroll-two");
+                if (url != null) {
+                    var urlContainer = document.createElement("a");
+                    urlContainer.href = url;
+                    var urlContainer2 = document.createElement("a");
+                    urlContainer2.href = url;
+                    urlContainer.className = "item";
+                    urlContainer2.className = "item";
+                    urlContainer.appendChild(sponsorImage);
+                    urlContainer2.appendChild(sponsorImage2);
+
+                    sponsorImage.className = "item";
+                    sponsorImage2.className = "item";
+                    firstLoop.appendChild(urlContainer);
+                    secondLoop.appendChild(urlContainer2);
+                } else {
+                    sponsorImage.className = "item";
+                    sponsorImage2.className = "item";
+                    firstLoop.appendChild(sponsorImage);
+                    secondLoop.appendChild(sponsorImage2);
+                }
             } else {
-                sponsorImage.className = "item";
-                sponsorImage2.className = "item";
-                firstLoop.appendChild(sponsorImage);
-                secondLoop.appendChild(sponsorImage2);
+                var alt = parsedData[i].alt;
+                var url = parsedData[i].url;
+                var firstLoop = document.getElementById("scroll-one");
+                var secondLoop = document.getElementById("scroll-two");
+                if (url != null) {
+                    var urlContainer = document.createElement("a");
+                    urlContainer.textContent = alt;
+                    urlContainer.href = url;
+                    var urlContainer2 = document.createElement("a");
+                    urlContainer2.textContent = alt;
+                    urlContainer2.href = url;
+                    urlContainer.className = "item";
+                    urlContainer2.className = "item";
+                
+                    firstLoop.appendChild(urlContainer);
+                    secondLoop.appendChild(urlContainer2);
+                } else {
+                    var urlContainer = document.createElement("a");
+                    var urlContainer2 = document.createElement("a");
+                    urlContainer.textContent = alt;
+                    urlContainer2.textContent = alt;
+                    urlContainer.className = "item";
+                    urlContainer2.className = "item";
+                
+                    firstLoop.appendChild(urlContainer);
+                    secondLoop.appendChild(urlContainer2);
+                }
             }
         } 
     } catch (error) {
