5b75ec6b2052148cbec52d5ff8cacdb4cbf07428
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2024-06-06 23:08:02 +0000

    Variables

--- JS/blogEntry.js
+++ JS/blogEntry.js
@@ -1,21 +1,21 @@
-const sponsorListURL =  "https://garinhamburg.github.io/Team2550.github.io/Blog/BlogEntries.json";
+const BlogEntriesURL =  "https://garinhamburg.github.io/Team2550.github.io/Blog/BlogEntries.json";
 async function serveBlog() {
     try {
-        const fetched = await fetch(sponsorListURL);
-        const jsonData = await fetched.text();
-        const parsedData = JSON.parse(jsonData);
+        const fetchedBlog = await fetch(BlogEntriesURL);
+        const blogJsonData = await fetchedBlog.text();
+        const parsedBlogData = JSON.parse(blogJsonData);
         // This is also bad,
         // At some point style will need to be added.
         //
         // -G
-        for (var i = 0; i < parsedData.length; i++) {
-            if (window.location.href.includes(parsedData[i].date)) {
-                var title = parsedData[i].title;
+        for (var i = 0; i < parsedBlogData.length; i++) {
+            if (window.location.href.includes(parsedBlogData[i].date)) {
+                var title = parsedBlogData[i].title;
                 document.title = `Skynet | ${title}`;
                 var content = document.createElement("div");
                 content.className = "blog";
 
-                var fetchedEntry = await fetch("https:///garinhamburg.github.io/Team2550.github.io/Blog/BlogEntries/" + parsedData[i].date + ".md");
+                var fetchedEntry = await fetch("https:///garinhamburg.github.io/Team2550.github.io/Blog/BlogEntries/" + parsedBlogData[i].date + ".md");
                 var data = await fetchedEntry.text();
                 var text = document.createElement("p");
                 text.textContent = data;
